cdbac96bdcbdae0bb438bdfe5fc1587fa3dc6967
Jean Pierre Dudey <jeandudey@hotmail.com>
2020-06-20 16:09:53 -0500

    {mio,tokio}-udev: apply copyright
    Signed-off-by: Jean Pierre Dudey <jeandudey@hotmail.com>

--- tokio-udev/examples/usb_hotplug.rs
+++ tokio-udev/examples/usb_hotplug.rs
@@ -1,3 +1,13 @@
+// Copyright 2020 Jean Pierre Dudey. See the LICENSE-MIT and
+// LICENSE-APACHE files at the top-level directory of this
+// distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
 use futures_util::future::ready;
 use futures_util::stream::StreamExt;
 use tokio_udev::{Context, MonitorBuilder};
--- tokio-udev/src/lib.rs
+++ tokio-udev/src/lib.rs
@@ -1,4 +1,12 @@
-#![cfg(target_os = "linux")]
+// Copyright 2020 Jean Pierre Dudey. See the LICENSE-MIT and
+// LICENSE-APACHE files at the top-level directory of this
+// distribution.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
 
 //! # tokio-udev
 //!
@@ -19,6 +27,8 @@
 //! ```rust
 //! use tokio_udev;
 //! ```
+
+#![cfg(target_os = "linux")]
 
 pub use mio_udev::{
     Attribute, Attributes, Context, Device, Enumerator, Event, EventType,
